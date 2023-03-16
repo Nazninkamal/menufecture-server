ccc2474bb7d52aa30c47c42400ddda7433138203
Your Name <you@example.com>
2023-03-16 19:49:33 +0600

    code push

--- Routes/material.route.js
+++ Routes/material.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { createMaterial, getMaterial } = require("../controllers/material.controller");
-
+const verifyToken = require("../Middleware/verifyToken");
+const authorization = require("../Middleware/authorization");
 
 const router = express.Router();
 
-router.post("/create-material", createMaterial);
-router.get("/get-material", getMaterial);
+router.post("/create-material",verifyToken,authorization("supplier"), createMaterial);
+router.get("/get-material",verifyToken,authorization("supplier"), getMaterial);
 
 module.exports = router;
